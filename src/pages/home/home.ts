7ef1710d81deeac19d2bce3eac0906869e0c9a71
DH <hassleymathebula@gmail.com>
2018-04-17 05:28:28 +0200

    Locate Resturant

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,80 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Geolocation ,GeolocationOptions ,Geoposition ,PositionError } from '@ionic-native/geolocation';
 
+declare var google;
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+//memebers for options and current position
+  options : GeolocationOptions;
+  currentPos : Geoposition
+  @ViewChild('map') mapElement: ElementRef;
+  map: any;
 
+//Inject Geolocation via constructor
+  constructor(public navCtrl: NavController, private geolocation : Geolocation) {
   }
+
+//Call the getUserPosition when the view did enter
+ionViewDidEnter()
+{this.getUserPosition();}
+
+//Create maps
+addMap(lat,long){
+
+    let latLng = new google.maps.LatLng(lat, long);
+
+    let mapOptions = {
+    center: latLng,
+    zoom: 15,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+    }
+
+    this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
+    this.addMarker();
+
+}
+
+//Add Marker
+addMarker(){
+
+    let marker = new google.maps.Marker({
+    map: this.map,
+    animation: google.maps.Animation.DROP,
+    position: this.map.getCenter()
+    });
+
+    let content = "<p>This is your current position !</p>";
+    let infoWindow = new google.maps.InfoWindow({
+    content: content
+    });
+
+    google.maps.event.addListener(marker, 'click', () => {
+    infoWindow.open(this.map, marker);
+    });
+
+}
+
+//getUserPosition method - display the map based on the current user position
+getUserPosition(){
+    this.options = {
+    enableHighAccuracy : false
+    };
+    this.geolocation.getCurrentPosition(this.options).then((pos : Geoposition) => {
+
+        this.currentPos = pos;
+
+        console.log(pos);
+        this.addMap(pos.coords.latitude,pos.coords.longitude);
+
+    },(err : PositionError)=>{
+        console.log("error : " + err.message);
+    ;
+    })
+}
 
 }
