455966215697988f66be63359d7cd104d0da37dc
DH <hassleymathebula@gmail.com>
2018-04-17 17:37:15 +0200

    Find Resturants

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -14,6 +14,7 @@ export class HomePage {
   currentPos : Geoposition
   @ViewChild('map') mapElement: ElementRef;
   map: any;
+  places : Array<any> ;
 
 //Inject Geolocation via constructor
   constructor(public navCtrl: NavController, private geolocation : Geolocation) {
@@ -23,7 +24,7 @@ export class HomePage {
 ionViewDidEnter()
 {this.getUserPosition();}
 
-//Create maps
+//Create map
 addMap(lat,long){
 
     let latLng = new google.maps.LatLng(lat, long);
@@ -35,6 +36,15 @@ addMap(lat,long){
     }
 
     this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
+
+    this.getRestaurants(latLng).then((results : Array<any>)=>{
+        this.places = results;
+        for(let i = 0 ;i < results.length ; i++)
+        {
+            this.createMarker(results[i]);
+        }
+    },(status)=>console.log(status));
+
     this.addMarker();
 
 }
@@ -75,6 +85,40 @@ getUserPosition(){
         console.log("error : " + err.message);
     ;
     })
+}
+
+//Get Restaurants
+getRestaurants(latLng)
+{
+    var service = new google.maps.places.PlacesService(this.map);
+    let request = {
+        location : latLng,
+        radius : 8047 ,
+        types: ["restaurant"]
+    };
+    return new Promise((resolve,reject)=>{
+        service.nearbySearch(request,function(results,status){
+            if(status === google.maps.places.PlacesServiceStatus.OK)
+            {
+                resolve(results);
+            }else
+            {
+                reject(status);
+            }
+
+        });
+    });
+
+}
+
+//Create marker
+createMarker(place)
+{
+    let marker = new google.maps.Marker({
+    map: this.map,
+    animation: google.maps.Animation.DROP,
+    position: place.geometry.location
+    });
 }
 
 }
